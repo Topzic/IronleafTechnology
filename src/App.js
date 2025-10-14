5f6ee9557c89b4dfd2ecd1edb421065ab9fb7024
Topzic <support@naturego.ca>
2025-10-14 10:16:20 -0400

    fix: Change Router from BrowserRouter to HashRouter for improved routing compatibility

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Suspense } from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import { HelmetProvider } from "react-helmet-async";
 import "./App.css";
 import Navbar from "./components/Navbar";
